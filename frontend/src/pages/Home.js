90bc86ecf1ebef328fc1d2f82ac1912ef4757f9f
Liam Byrne <lbyr117@aucklanduni.ac.nz>
2025-08-19 10:32:29 +1200

    created navbar component with all required links

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../components/ui/Navbar';
 
 const HomePage = () => {
   return (
     <div className="bg-white">
+      <Navbar />
       <h1>Home</h1>
     </div>
   );
