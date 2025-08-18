b7e57e92fdc47d36e25b253008980a4514e98aae
nadiaaskari <nask472@aucklanduni.ac.nz>
2025-08-18 18:12:02 +1200

    Added route to Report Page in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,7 @@ import HomePage from './pages/Home';
 import FeedPage from './pages/Feed';
 import ProfilePage from './components/ProfilePage';
 import ItemDetailPage from './pages/ItemDetail';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ReportPage from './Pages/ReportPage';
+import ReportPage from './pages/ReportPage';
 
 
 
@@ -20,15 +18,7 @@ function App() {
           <Route path="/feed" element={<FeedPage />} />
           <Route path="/items/:id" element={<ItemDetailPage />} />
           <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </div>
-    </Router>
-    <Router>
-      <div className="p-4">
-        <Routes>
-          <Route path="/items/new" element={<ReportPage />} />
-          <Route path="/" element={<p>Welcome to the homepage! 🏠</p>} />
-          <Route path="/items" element={<p>View lost and found items!</p>} />
+           <Route path="/items/new" element={<ReportPage />} />
         </Routes>
       </div>
     </Router>
