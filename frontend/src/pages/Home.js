c69dba9bb2afddbeba6dda65ccb3cc94c3c77947
Liam Byrne <lbyr117@aucklanduni.ac.nz>
2025-08-19 12:09:38 +1200

    created footer component for all pages

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../components/ui/Navbar';
+import Footer from '../components/ui/Footer';
 
 const HomePage = () => {
   return (
     <div className="bg-white">
       <Navbar />
       <h1>Home</h1>
+      <Footer />
     </div>
   );
 };
