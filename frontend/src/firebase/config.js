89cdb9f8cb4a19c0b13bd9d6b0668ef4b977add8
Rudra <rpat943@aucklanduni.ac.nz>
2025-08-19 13:18:46 +1200

    fixed linter issue which fails CI/CD test

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -19,5 +19,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const db = getFirestore(app);
+const db = getFirestore(app);
 export { app, db, analytics };
